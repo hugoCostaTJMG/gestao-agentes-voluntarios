31e273b2c54d43a92bf5926dadbe2b867165c620
HugoCosta <hugo.guilhermecosta@globalhitss.com.br>
2025-08-20 16:42:30 -0300

    .

--- frontend/agentes-frontend/src/app/shared/components/buttons/button/button.component.ts
+++ frontend/agentes-frontend/src/app/shared/components/buttons/button/button.component.ts
@@ -1,15 +1,16 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { NgClass } from '@angular/common';
+import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
+import { NgClass, NgIf } from '@angular/common';
 import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-button',
   standalone: true,
-  imports: [NgClass, RouterLink],
+  imports: [NgClass, NgIf, RouterLink],
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss']
 })
-export class ButtonComponent {
+export class ButtonComponent implements OnInit {
   @Input() label: string = '';
   @Input() type: 'primary' | 'secondary' | 'danger' | 'ghost' = 'primary';
   @Input() size: 'sm' | 'md' | 'lg' = 'md';
@@ -18,8 +19,13 @@ export class ButtonComponent {
   @Input() iconRight?: string;
   @Input() buttonType: 'button' | 'submit' | 'reset' = 'button';
   @Input() routerLink?: string | any[];
+  @Input() ariaLabel?: string; 
 
   @Output() clicked = new EventEmitter<Event>();
+
+  ngOnInit(): void {
+    console.log('👉 iconLeft recebido:', this.iconLeft);
+  }
 
   onClick(event: Event): void {
     if (this.disabled) {
